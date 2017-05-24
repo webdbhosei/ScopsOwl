ce07dc2676b427b00e25914cd553d5e23c799866
kimmungai <kimpita9@gmail.com>
2017-05-24 20:43:47 +0900

    Action cable

---
+++ db/migrate/20170524105544_create_messages.rb
@@ -0,0 +1,10 @@
+class CreateMessages < ActiveRecord::Migration[5.0]
+  def change
+    create_table :messages do |t|
+      t.text :body
+      t.references :chat_room, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
