516b54bcfa3f79da35fcfc4b57c495cb08181587
rhezaharliman <rheza21.h@gmail.com>
2017-06-22 02:37:16 +0900

    rh21_imp : add column index to db

--- db/migrate/20170614200133_add_user_id_to_rh21_threads.rb
+++ db/migrate/20170614200133_add_user_id_to_rh21_threads.rb
@@ -1,4 +1,5 @@
 class AddUserIdToRh21Threads < ActiveRecord::Migration[5.0]
   def change
+    add_index :rh21_threads, :user_id
   end
 end
--- db/migrate/20170614200349_add_rh21_language_id_to_rh21_posts.rb
+++ db/migrate/20170614200349_add_rh21_language_id_to_rh21_posts.rb
@@ -1,4 +1,5 @@
 class AddRh21LanguageIdToRh21Posts < ActiveRecord::Migration[5.0]
   def change
+    add_index :rh21_posts, :rh21_language_id
   end
 end
