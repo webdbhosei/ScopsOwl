fffe9c24d59ffcfc3894c61ea15a363590aafa48
kimmungai <kimpita9@gmail.com>
2017-06-21 21:23:53 +0900

    Okane db connection

--- app/controllers/okane_controller.rb
+++ app/controllers/okane_controller.rb
@@ -1,4 +1,5 @@
 class OkaneController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
   def index
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615021809) do
+ActiveRecord::Schema.define(version: 20170621121930) do
 
   create_table "chat_groups", force: :cascade do |t|
     t.string   "name"
@@ -74,6 +74,13 @@ ActiveRecord::Schema.define(version: 20170615021809) do
     t.integer  "quality"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "smpl_chats", force: :cascade do |t|
